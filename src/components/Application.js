144700c0ad319f57ea933d3b75569cd3181d9f36
Taylor Noj <taylornoj@gmail.com>
2021-12-07 21:54:15 -0500

    store day state in application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
+
 
 const days = [
   {
@@ -21,6 +22,7 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
@@ -33,8 +35,8 @@ export default function Application(props) {
 <nav className="sidebar__menu">
   <DayList
     days={days}
-    day={"Monday"}
-    setDay={day => console.log(day)}
+    day={day}
+    setDay={setDay}
   />
 </nav>
 <img
