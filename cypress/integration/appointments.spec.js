16c24576a054b53091f42174cf3fac65f0ec9f77
Taylor Noj <taylornoj@gmail.com>
2021-12-15 17:21:36 -0500

    begin the cancel appointment test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -35,6 +35,8 @@ describe("Appointments", () => {
 
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Tori Malcolm");
-
   });
+  it("should cancel an interview", () => {
+    
+  })
  });
