c19b6e8c548e01df7228eb33de56f6d5d9927655
Taylor Noj <taylornoj@gmail.com>
2021-12-15 17:32:45 -0500

    confirms that the appointment card showing Archie Cohen does not exist

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -44,5 +44,8 @@ describe("Appointments", () => {
 
     cy.contains("Deleting").should("exist");
     cy.contains("Deleting").should("not.exist");
-  })
+
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
+  });
  });
