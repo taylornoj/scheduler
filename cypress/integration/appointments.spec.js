575147b5a02a5f852a3c6099f707b9341d806e97
Taylor Noj <taylornoj@gmail.com>
2021-12-15 16:54:36 -0500

    test for clicking the save button

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -7,5 +7,6 @@ describe("Appointments", () => {
       .click();
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
+    cy.contains("Save").click();
   });
 });
