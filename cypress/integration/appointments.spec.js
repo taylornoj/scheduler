b3acfc0c301c4654c58fc6e51cf2da72aad02288
Taylor Noj <taylornoj@gmail.com>
2021-12-15 17:10:06 -0500

    add edit test and force click

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -20,4 +20,10 @@ describe("Appointments", () => {
    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
    cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+
+  });
  });
