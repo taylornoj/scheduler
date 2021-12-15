803ae4281c4c1a2e35434ed8ee129450745569cd
Taylor Noj <taylornoj@gmail.com>
2021-12-15 17:24:16 -0500

    am able to click the delete button on apointment

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -37,6 +37,8 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Tori Malcolm");
   });
   it("should cancel an interview", () => {
-    
+    cy.get("[alt=Delete]")
+      .first()
+      .click({ force: true });
   })
  });
